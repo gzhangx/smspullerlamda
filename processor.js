583bea5d604a83d1bf583174cbf61817c501879d
Gang Zhang <gzhangx@hotmail.com>
2021-12-03 00:17:16 -0500

    'test'

---
+++ processor.js
@@ -0,0 +1,23 @@
+const db = require('./db');
+async function process(body) {
+    if (!body.username) {
+        return {
+            error:'No username',
+        }
+    }
+
+    const user = await db.getOneByName(process.env.USER_TABLE_NAME, 'username', body.username);
+    if (!user) {
+        return {
+            error:'User not found'
+        }
+    }
+
+    return {
+        ...user;
+    }
+}
+
+module.exports = {
+    process,
+}
