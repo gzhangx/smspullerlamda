99f684f1ed42dd80c8dde52c5d694ba56d9a3b60
Gang Zhang <gzhangx@hotmail.com>
2021-12-03 00:16:36 -0500

    'test'

---
+++ db.js
@@ -0,0 +1,122 @@
+const AWS = require('aws-sdk');
+AWS.config.update({ region: 'us-east-2' });
+const docClient = new AWS.DynamoDB.DocumentClient();
+
+async function getAll(params, onData) {
+    const onScan = (err, data) => {
+        if (err) {
+            onData(err);
+        } else {
+            onData(err, data); //data.items            
+            // continue scanning if we have more items
+            if (typeof data.LastEvaluatedKey != "undefined") {
+                console.log("Scanning for more...");
+                params.ExclusiveStartKey = data.LastEvaluatedKey;
+                docClient.scan(params, onScan);
+            }
+        }
+    };
+    docClient.scan(params, onScan)
+}
+
+async function getAllByTable(TableName, FilterExpression, ExpressionAttributeValues, onData) {
+    return getAll({
+        TableName,
+        FilterExpression,
+        ExpressionAttributeValues,
+    }, onData);
+}
+
+async function getOneByName(TableName, name, value) {
+    return new Promise((resolve, reject) => getAll({
+        TableName,
+        FilterExpression: `${name}=:name`,
+        ExpressionAttributeValues: {
+            ':name': value
+        },
+    }, (err, data) => {
+        if (err) reject(err);
+        else resolve(data.Items[0]);
+    }));
+}
+
+async function addData(TableName, Item) {
+    return await docClient.put({
+        TableName,
+        Item
+    }).promise();
+}
+
+async function updateData(TableName, id,
+    UpdateExpression,
+    ExpressionAttributeValues
+) {
+    return await docClient.update({
+        TableName,
+        Key: { id },
+        UpdateExpression,
+        ExpressionAttributeValues,
+        ReturnValues: 'UPDATED_NEW',
+    }).promise();
+}
+
+async function deleteData(TableName, id) {
+    return await docClient.delete({
+        TableName,
+        Key: { id },
+    }).promise();
+}
+
+async function test() {
+    const customerTable = 'CustomerUser-kh7dnrwmljerze6banc6qglajq-staging';
+    const addres = await addData(customerTable, {
+        id: 'test1',
+        email: 'gg@zz.com',
+        phone: '411111111',
+        name: 'gg',
+    });
+    console.log(addres);
+    const updateRes = await updateData(customerTable,
+        'test1', 'set email=:email', {
+        ":email": 'newemail'
+    });
+    console.log(updateRes);
+    const delres = await deleteData(customerTable, 'test1');
+    console.log('deleted');
+    console.log(delres);
+    await getAllByTable(customerTable, null, null, (err, data) => {
+        if (err) {
+            console.log('error');
+            console.log(err);
+        } else {
+            console.log('data for cust')
+            console.log(data);
+        }
+    });
+
+    await getAllByTable('BusinessUser-kh7dnrwmljerze6banc6qglajq-staging',
+        'username=:usr', {
+        ':usr': 'ericktest6'
+    }, (err, data) => {
+        if (err) {
+            console.log('errr');
+            console.log(err);
+        } else {
+            console.log('data for bus user');
+            console.log(data);
+        }
+    });
+
+    const getoneres = await getOneByName('BusinessUser-kh7dnrwmljerze6banc6qglajq-staging', 'username', 'ericktest6');
+    console.log('getoneres');
+    console.log(getoneres);
+}
+
+module.exports = {
+    getAll,
+    getAllByTable,
+    addData,
+    updateData,
+    deleteData,
+    getOneByName,
+}
--- index.js
+++ index.js
@@ -1,16 +1,31 @@
-const AWS = require('aws-sdk')
+const AWS = require('aws-sdk');
+const processor = require('./processor');
 
 const api = new AWS.ApiGatewayManagementApi({
   endpoint: '6jpn0ivmf6.execute-api.us-east-2.amazonaws.com/production'
 })
 
+function parseBody(body) {
+    if (!body) return {
+        error:'Null body'
+    };
+    try {
+        return {
+            ok: JSON.parse(body),
+        }
+    } catch {
+        return {
+            error: `failed to parse body:${body}`,
+        }
+    }
+}
 exports.handler = async (event) => {
     // TODO implement
     console.log('got event');
     console.log(event);
-    const route = event.requestContext?.routeKey;
-    const connectionId = event.requestContext?.connectionId;
-    switch(route) {
+    const { routeKey, connectionId } = event.requestContext || {};
+    const body = parseBody(event.body);    
+    switch (routeKey) {
         case '$connect':
             console.log('connected');
             break;
@@ -19,25 +34,24 @@ exports.handler = async (event) => {
             break;
         case '$default':
         case 'message':
-            console.log('message receive ');
-            await replyToMessage('sending rsp ' + new Date().toISOString(), connectionId)
+            console.log(`message receive ${routeKey}`);
+            await replyToMessage(body.ok ?await processor.process(body.ok):body, connectionId)
             break;
         default:
-            console.log('unknown route ' + route);
+            console.log('unknown route ' + routeKey);
         break;
     }
     const response = {
         statusCode: 200,
-        body: JSON.stringify('Hello from Lambda sms puller!'),
+        body,
     };
     return response;
 };
 
 async function replyToMessage(response, connectionId) {
-    const data = { message: response }
     const params = {
       ConnectionId: connectionId,
-      Data: Buffer.from(JSON.stringify(data))
+        Data: Buffer.from(JSON.stringify(response))
     }
 
     return api.postToConnection(params).promise()
